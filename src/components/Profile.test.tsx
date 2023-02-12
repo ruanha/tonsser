7346dd9efdc0a7f7599e74a5478927c41c51de4d
Rune Hartvig <rune_hartvig@hotmail.com>
2023-02-12 15:06:03 +0100

    add tests for validation error

--- src/components/Profile.test.tsx
+++ src/components/Profile.test.tsx
@@ -1,4 +1,5 @@
 import { cleanup, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Profile from "./Profile";
 
 afterEach(cleanup);
@@ -10,6 +11,31 @@ test("renders profile form", () => {
   expect(screen.getByLabelText("Club")).toHaveValue(profile.club);
   expect(screen.getByLabelText("Role")).toHaveValue(profile.role);
   expect(screen.getByLabelText("Bio")).toHaveValue(profile.bio);
+});
+
+test("displays error message when password is too short and submit button is clicked", async () => {
+  render(<Profile profile={profile} />);
+  userEvent.type(screen.getByLabelText("New password"), "123");
+  userEvent.click(screen.getByRole("button", { name: "Save" }));
+  expect(
+    screen.getByText("Password must be at least 8 characters long.")
+  ).toBeVisible();
+});
+
+test("displays error message when passwords do not match and submit button is clicked", async () => {
+  render(<Profile profile={profile} />);
+  userEvent.type(screen.getByLabelText("New password"), "12345678");
+  userEvent.type(screen.getByLabelText("Repeat new password"), "123456789");
+  userEvent.click(screen.getByRole("button", { name: "Save" }));
+  expect(screen.getByText("Passwords do not match.")).toBeVisible();
+});
+
+test("does not display error message when password is valid and submit button is clicked", async () => {
+  render(<Profile profile={profile} />);
+  userEvent.type(screen.getByLabelText("New password"), "12345678");
+  userEvent.type(screen.getByLabelText("Repeat new password"), "12345678");
+  userEvent.click(screen.getByRole("button", { name: "Save" }));
+  expect(screen.queryByText("Passwords do not match.")).not.toBeInTheDocument();
 });
 
 const profile = {
