3fa35b4bb896510dd32c93924edeed85e7e9c803
Rune Hartvig <rune_hartvig@hotmail.com>
2023-02-12 10:29:56 +0100

    init Profile component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import styles from "./App.module.css";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div>
       <header className={styles.AppHeader}>HEADER</header>
+      <main>
+        <Profile />
+      </main>
     </div>
   );
 }
