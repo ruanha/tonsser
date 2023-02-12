79bb843b52c907664c443ead9fd1a6718aa88e8b
Rune Hartvig <rune_hartvig@hotmail.com>
2023-02-12 10:41:10 +0100

    init form fields, labels and placeholders

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,8 +2,45 @@ import styles from "./Profile.module.css";
 
 export default function Profile() {
   return (
-    <div>
-      <h1>Profile</h1>
-    </div>
+    <form className={styles.form}>
+      <div className={styles.formGroup}>
+        <label htmlFor="first-name">First name</label>
+        <input
+          type="text"
+          name="first-name"
+          id="first-name"
+          placeholder="First name"
+        />
+        <label htmlFor="last-name">Last name</label>
+        <input
+          type="text"
+          name="last-name"
+          id="last-name"
+          placeholder="Last name"
+        />
+        <label htmlFor="club">Club</label>
+        <input type="text" name="club" id="club" placeholder="Eg. Tonsser FC" />
+        <label htmlFor="role">Role</label>
+        <input type="text" name="role" id="role" placeholder="Eg. Striker" />
+      </div>
+      <div className={styles.formGroup}>
+        <label htmlFor="bio">Bio</label>
+        <textarea name="bio" className={styles.textarea}></textarea>
+        <label htmlFor="password">New Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+        />
+        <label htmlFor="repeat-password">Repeat password</label>
+        <input
+          type="password"
+          name="repat-password"
+          id="repeat-password"
+          placeholder="Repeat password"
+        />
+      </div>
+    </form>
   );
 }
