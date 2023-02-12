72deea98cd8d52b8cc7b437b6647374754d34313
Rune Hartvig <rune_hartvig@hotmail.com>
2023-02-12 14:15:43 +0100

    refactor: oneline the validation functions

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -73,13 +73,10 @@ export default function Profile({ profile }: { profile: any }) {
     }
   }
 
-  function passwordIsValid(password: string) {
-    return password.length >= 8;
-  }
+  const passwordIsValid = (password: string) => password.length >= 8;
 
-  function repeatIsValid(password: string, repeatPassword: string) {
-    return password === repeatPassword;
-  }
+  const repeatIsValid = (password: string, repeatPassword: string) =>
+    password === repeatPassword;
 
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
@@ -185,4 +182,37 @@ function mockFetch(_url: string, _options: any): Promise<any> {
       });
     }, 2000);
   });
+}
+
+function FormField({
+  label,
+  type,
+  name,
+  placeholder,
+  value,
+  onChange,
+  error,
+}: {
+  label: string;
+  type: string;
+  name: string;
+  placeholder: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  error: string;
+}) {
+  return (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <input
+        type={type}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+      {error && <p className="error">{error}</p>}
+    </div>
+  );
 }
