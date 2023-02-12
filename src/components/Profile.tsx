6d4b8b7b3ce8939be21d05c4d7ca0beb1b4cb7bd
Rune Hartvig <rune_hartvig@hotmail.com>
2023-02-12 11:27:08 +0100

    style input field states

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -17,6 +17,7 @@ export default function Profile() {
           name="last-name"
           id="last-name"
           placeholder="Last name"
+          readOnly
         />
         <label htmlFor="club">Club</label>
         <input type="text" name="club" id="club" placeholder="Eg. Tonsser FC" />
